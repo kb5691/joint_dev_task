bbd9b23496fe8de89e6fde83ea503f1ce05cbc95
oshiro <kaito.o.0628@gmail.com>
2022-07-08 21:41:29 +0900

    mapメソッド内で&:を使用するように修正

--- task.rb
+++ task.rb
@@ -71,10 +71,10 @@ def q8
 
   # 以下に回答を記載
   # 文字列の先頭を大文字に変換
-  programming_languages.map { |programming_language| programming_language.capitalize! }
+  programming_languages.map(&:capitalize!)
 
   # 文字列を大文字に変換
-  upper_case_programming_languages = programming_languages.map { |programming_language| programming_language.upcase }
+  upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
   p programming_languages
