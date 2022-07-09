b5b35e270de2ec1c3118b5e85f23f8e72dd5e18f
oshiro <kaito.o.0628@gmail.com>
2022-07-09 14:00:58 +0900

    Q10~Q12を回答

--- task.rb
+++ task.rb
@@ -95,13 +95,25 @@ def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each do |food|
+    if food.include?("うに")
+      puts "#{food}:好物です"
+    else
+      puts "#{food}:まぁまぁ好きです"
+    end
+  end
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
+  # 配列を1次元配列に変更し、重複した文字列を削除
+  sports.flatten!.uniq!
+  # 繰り返し処理を行う
+  sports.each.with_index(1) do |sport, i|
+    puts "No#{i} #{sport}"
+  end
 
 end
 
@@ -109,6 +121,7 @@ def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
+  puts data[:user][:name]
 
 end
 
